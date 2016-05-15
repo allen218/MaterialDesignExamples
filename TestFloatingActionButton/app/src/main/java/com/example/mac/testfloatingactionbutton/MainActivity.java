30fa8c00c9801f40115957fa582bfaf8b9cb98af
allen <wangcongccnp@126.com>
2016-05-15 13:23:19 +0800

    添加FloatingActionButton

---
+++ TestFloatingActionButton/app/src/main/java/com/example/mac/testfloatingactionbutton/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.mac.testfloatingactionbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initView();
+    }
+
+    private void initView() {
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+}
